ee7ec50f28058928fa19e82bd66edf74b0ccd06f
Rohald van Merode <rohald_89@live.nl>
2021-05-15 18:45:51 +0200

    added terser plugin for minimizing JS

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const webpack = require('webpack');
 
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const { DefaultDeserializer } = require('v8');
 
 const IS_DEVELOPMENT = process.env.NODE_ENV === 'dev';
 
@@ -109,5 +111,9 @@ module.exports = {
         exclude: /node_modules/,
       },
     ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
   },
 };
